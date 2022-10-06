e8ed1bfc8290576d1361acbec2f7905277391daa
AIRed0xs <codejblp@gmail.com>
2022-10-06 09:19:08 -0400

    first commit

---
+++ NmapScanner.py
@@ -0,0 +1,29 @@
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("<--------------------->")
+print("Simple Nmap Scanner")
+print("<--------------------->")
+
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+resp = input("""\nPlease enter the type of scan you want to run
+                1)SYN ACK Scan
+                2)UDP Scan
+                3)Comprehensive Scan
+                4)Decoy Scan \n""")
+print("You have selected option: ", resp)
+resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp'],'4':['-vv -D RND,RND,ME,RND -sV -sC -A -T1','tcp']}
+if resp not in resp_dict.keys():
+    print("enter a valid option")
+else:
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the port range to scan, the last part is the scan type
+    print(scanner.scaninfo())
+    if scanner.scaninfo()=='up':
+        print("Scanner `Status: ",scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  #display all open ports
